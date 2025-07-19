f59dfb5b76667b524bb146c44ea7fe6309d40e90
khan632 <rmustafa632@gmail.com>
2025-07-19 11:40:53 +0530

    Added interactive component without using state

---
+++ steps/src/App.jsx
@@ -0,0 +1,5 @@
+import StepsWOState from "./component/StepsWOState";
+
+export default function App () {
+  return <StepsWOState />
+}
---
+++ steps/src/constant.js
@@ -0,0 +1,5 @@
+export const messages = [
+  "Learn React ⚛",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
