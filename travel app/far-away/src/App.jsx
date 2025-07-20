4b0b3581726dce7391fd9634cd19865bdc07c0b1
khan632 <rmustafa632@gmail.com>
2025-07-20 12:39:07 +0530

    added theory difference between props and state

--- travel app/far-away/src/App.jsx
+++ travel app/far-away/src/App.jsx
@@ -13,3 +13,20 @@ export default function App () {
     </div>
   )
 }
+
+/*
+STATE vs PROPS
+States: 
+1. it's an internal data, owned by components.
+2. it is known as component "memory", used to handle data inside component independently.
+3. can be updated by the components itself.
+4. updating a state causes component to re-render
+5. used to make components more interactive.
+
+Props:
+1. It's an external data, owned by parents components.
+2. Similar to function's parameter.
+3. Read only data passed from parents to children, children can only use this data not able to modify it.
+4. Receiving new props causes component to re-render, usually when parent props is updated or modified.
+5. used by parent component to configure child components.
+*/
