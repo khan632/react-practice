389d2a185b9e8fd9316fb0fe9497a3a2d1034895
khan632 <rmustafa632@gmail.com>
2025-07-20 12:29:07 +0530

    create controlled form component

--- travel app/far-away/src/components/Form.jsx
+++ travel app/far-away/src/components/Form.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!description) return;
+    
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  
   return (
-    <div className="add-form">
+    <form className="add-form" onClick={handleSubmit}>
         <h3>What di you need for your 😍 trip ?</h3>
-    </div>
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {
+            Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+              <option value={num} key={num}>
+                {num}
+              </option>
+            ))
+          }
+        </select>
+        <input type="text" placeholder="item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+        <button>Add</button> 
+    </form>
   )
 }
 
